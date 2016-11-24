85f4919123d30cb89aa7015c866ac94474f4d72a
Nicholas Laughter <nicholaslaughter@LaughterBookPro.PK5001Z>
2016-11-24 10:25:07 -0800

    Conversion to Swift 3 with errors

--- One More Pun/PunController.swift
+++ One More Pun/PunController.swift
@@ -45,39 +45,39 @@ class PunController {
         return newPun
     }
     
-    func createPun(body: String) {
+    func createPun(_ body: String) {
         let punIdentifier = FirebaseController.ref.child(punsPathString).childByAutoId().key
         var pun = Pun(body: body, reportedCount: 0, identifier: punIdentifier)
         pun.save()
     }
     
-    func observePuns(completion: (puns: [Pun]) -> Void) {
+    func observePuns(_ completion: @escaping (_ puns: [Pun]) -> Void) {
         let punsRef = FirebaseController.ref.child(punsPathString)
-        punsRef.observeEventType(.Value, withBlock: { (data) in
-            guard let punsDict = data.value as? [String: [String: AnyObject]] else { completion(puns: []); return }
+        punsRef.observe(.value, with: { (data) in
+            guard let punsDict = data.value as? [String: [String: AnyObject]] else { completion([]); return }
             let unfilteredPuns = punsDict.flatMap { Pun(dictionary: $1, identifier: $0) }
             let filteredPuns = unfilteredPuns.filter { $0.reportedCount < 5 }
             let punsToDelete = unfilteredPuns.filter { $0.reportedCount >= 5 }
             for pun in punsToDelete {
                 self.deletePun(pun)
             }
-            completion(puns: filteredPuns)
+            completion(filteredPuns)
         })
     }
     
-    func deletePun(pun: Pun) {
+    func deletePun(_ pun: Pun) {
         guard let identifier = pun.identifier else { return }
         FirebaseController.ref.child(pun.endpoint).child(identifier).removeValue()
     }
     
-    func reportPun(pun: Pun) {
+    func reportPun(_ pun: Pun) {
         guard let identifier = pun.identifier else { return }
         let reports = (pun.reportedCount + 1)
-        let childUpdates: [NSObject: AnyObject] = ["/\(pun.endpoint)/\(identifier)/\(reportedCountKey)": reports]
+        let childUpdates: [AnyHashable: Any] = ["/\(pun.endpoint)/\(identifier)/\(reportedCountKey)": reports]
         FirebaseController.ref.updateChildValues(childUpdates)
     }
     
-    func getPunTextAndSubmitter(pun: Pun) -> String {
+    func getPunTextAndSubmitter(_ pun: Pun) -> String {
         if let submitter = pun.submitter {
             return "\(pun.body)\nSubmitted by \(submitter)"
         } else {
@@ -85,13 +85,13 @@ class PunController {
         }
     }
     
-    func getItemsToShare(pun: Pun, color: UIColor) -> [AnyObject] {
-        let point = CGPointMake(10, 10)
+    func getItemsToShare(_ pun: Pun, color: UIColor) -> [AnyObject] {
+        let point = CGPoint(x: 10, y: 10)
         let imageSize = CGSize(width: 1024, height: 1024)
         let imageWithColor = getImageWithColor(color, size: imageSize)
-        let image = textToImage(getPunTextAndSubmitter(pun), inImage: imageWithColor, atPoint: point)
+        let image = textToImage(getPunTextAndSubmitter(pun) as NSString, inImage: imageWithColor, atPoint: point)
         let comment = "Shared via One More Pun!\nhttp://tinyurl.com/OneMorePun"
-        return [image, comment]
+        return [image, comment as AnyObject]
     }
 }
 
@@ -99,28 +99,28 @@ extension PunController {
     
     // MARK: - Image Helper
     
-    func getImageWithColor(color: UIColor, size: CGSize) -> UIImage {
-        let rect = CGRectMake(0, 0, size.width, size.height)
+    func getImageWithColor(_ color: UIColor, size: CGSize) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
         UIGraphicsBeginImageContextWithOptions(size, false, 0)
         color.setFill()
         UIRectFill(rect)
-        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         return image
     }
     
-    func textToImage(drawText: NSString, inImage: UIImage, atPoint: CGPoint) -> UIImage {
+    func textToImage(_ drawText: NSString, inImage: UIImage, atPoint: CGPoint) -> UIImage {
         guard let font = UIFont(name: "CoolveticaRg-Regular", size: 100) else { return UIImage() }
-        let textColor: UIColor = UIColor.whiteColor()
+        let textColor: UIColor = UIColor.white
         UIGraphicsBeginImageContext(inImage.size)
         let textFontAttributes = [
             NSFontAttributeName: font,
             NSForegroundColorAttributeName: textColor,
             ]
-        inImage.drawInRect(CGRectMake(0, 0, inImage.size.width, inImage.size.height))
-        let rect: CGRect = CGRectMake(atPoint.x, atPoint.y, inImage.size.width, inImage.size.height)
-        drawText.drawInRect(rect, withAttributes: textFontAttributes)
-        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        inImage.draw(in: CGRect(x: 0, y: 0, width: inImage.size.width, height: inImage.size.height))
+        let rect: CGRect = CGRect(x: atPoint.x, y: atPoint.y, width: inImage.size.width, height: inImage.size.height)
+        drawText.draw(in: rect, withAttributes: textFontAttributes)
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         return newImage
     }
--- One More Pun/ViewController.swift
+++ One More Pun/ViewController.swift
@@ -27,14 +27,14 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
     var retrievingFromNetwork: Bool = false {
         didSet {
-            UIApplication.sharedApplication().networkActivityIndicatorVisible = retrievingFromNetwork
+            UIApplication.shared.isNetworkActivityIndicatorVisible = retrievingFromNetwork
         }
     }
     
     func printFonts() {
-        for familyName in UIFont.familyNames() {
+        for familyName in UIFont.familyNames {
             print("\n-- \(familyName) \n")
-            for fontName in UIFont.fontNamesForFamilyName(familyName) {
+            for fontName in UIFont.fontNames(forFamilyName: familyName) {
                 print(fontName)
             }
         }
@@ -60,12 +60,12 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
         
         checkUserAndReloadData()
         
-        infoButtonColor.hidden = true
-        addPunButtonColor.hidden = true
+        infoButtonColor.isHidden = true
+        addPunButtonColor.isHidden = true
         
         let rate = RateMyApp.sharedInstance
         rate.appID = "1008575898"
-        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        DispatchQueue.main.async(execute: { () -> Void in
             rate.trackAppUsage()
         })
         
@@ -74,11 +74,11 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
     // MARK: - Pun Info Options
     
-    @IBAction func infoButtonTapped(sender: AnyObject) {
+    @IBAction func infoButtonTapped(_ sender: AnyObject) {
         showPunInfoActionSheet()
     }
     
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         getNewPunAndColor()
@@ -106,7 +106,7 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
         PunController.shared.observePuns { (puns) in
             self.retrievingFromNetwork = true
             PunController.shared.punsArray = puns
-            dispatch_async(dispatch_get_main_queue(), {
+            DispatchQueue.main.async(execute: {
                 self.retrievingFromNetwork = false
                 self.getNewPunAndColor()
             })
@@ -115,16 +115,16 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
     func showLoginSignUpView() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        guard let vc = storyboard.instantiateViewControllerWithIdentifier("LoginViewController") as? LoginTableViewController else { return }
-        self.presentViewController(vc, animated: true, completion: nil)
+        guard let vc = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as? LoginTableViewController else { return }
+        self.present(vc, animated: true, completion: nil)
     }
     
-    @IBAction func nextPunButton(sender: AnyObject) {
+    @IBAction func nextPunButton(_ sender: AnyObject) {
         getNewPunAndColor()
         punsReadCount += 1
     }
     
-    @IBAction func addPunButtonTapped(sender: AnyObject) {
+    @IBAction func addPunButtonTapped(_ sender: AnyObject) {
         checkIfCurrentUserIsNil({ 
             self.presentSubmitPunAlert(nil)
             }) { 
@@ -143,13 +143,13 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
         color = colorCollection.randomColor()
         view.backgroundColor = color
         punButtonColor.tintColor = color
-        infoButtonColor.hidden = false
+        infoButtonColor.isHidden = false
         infoButtonColor.tintColor = color
-        addPunButtonColor.hidden = false
+        addPunButtonColor.isHidden = false
         addPunButtonColor.tintColor = color
     }
     
-    func submitterLabelText(pun: Pun) -> String {
+    func submitterLabelText(_ pun: Pun) -> String {
         if let submitter = pun.submitter {
             return "Submitted by \(submitter)"
         } else {
@@ -157,7 +157,7 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
         }
     }
     
-    func checkIfCurrentUserIsNil(ifNotNil: () -> Void, ifNil: () -> Void) {
+    func checkIfCurrentUserIsNil(_ ifNotNil: () -> Void, ifNil: () -> Void) {
         if FIRAuth.auth()?.currentUser != nil {
             ifNotNil()
         } else {
@@ -167,131 +167,131 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
     // MARK: - AlertController
     
-    func presentSubmitPunAlert(storedPun: String?) {
-        let alert = UIAlertController(title: "Got a pun?", message: "Submitting puns is punderful.", preferredStyle: .Alert)
-        alert.addTextFieldWithConfigurationHandler { (punTextField) in
+    func presentSubmitPunAlert(_ storedPun: String?) {
+        let alert = UIAlertController(title: "Got a pun?", message: "Submitting puns is punderful.", preferredStyle: .alert)
+        alert.addTextField { (punTextField) in
             punTextField.placeholder = "Enter pun here"
             punTextField.text = storedPun
-            punTextField.autocorrectionType = .Yes
-            punTextField.autocapitalizationType = .Sentences
-            punTextField.spellCheckingType = .Yes
+            punTextField.autocorrectionType = .yes
+            punTextField.autocapitalizationType = .sentences
+            punTextField.spellCheckingType = .yes
         }
-        let submitAction = UIAlertAction(title: "Submit", style: .Default) { (_) in
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { (_) in
             guard let textFields = alert.textFields,
-                punTextField = textFields.first,
-                punText = punTextField.text where !punText.isEmpty else { return }
+                let punTextField = textFields.first,
+                let punText = punTextField.text, !punText.isEmpty else { return }
             self.presentSubmitPunConfirmationAlert(punText)
         }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alert.addAction(submitAction)
         alert.addAction(cancelAction)
-        presentViewController(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
     
-    func presentSubmitPunConfirmationAlert(punBody: String) {
-        let alert = UIAlertController(title: "All done?", message: "Be sure to check spelling and grammar! Here's how it looks:\n\(punBody)", preferredStyle: .Alert)
-        let submitAction = UIAlertAction(title: "Looks good!", style: .Default) { (_) in
+    func presentSubmitPunConfirmationAlert(_ punBody: String) {
+        let alert = UIAlertController(title: "All done?", message: "Be sure to check spelling and grammar! Here's how it looks:\n\(punBody)", preferredStyle: .alert)
+        let submitAction = UIAlertAction(title: "Looks good!", style: .default) { (_) in
             PunController.shared.createPun(punBody)
         }
-        let reEnterAction = UIAlertAction(title: "Re-enter", style: .Destructive) { (_) in
+        let reEnterAction = UIAlertAction(title: "Re-enter", style: .destructive) { (_) in
             self.presentSubmitPunAlert(punBody)
         }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alert.addAction(submitAction)
         alert.addAction(reEnterAction)
         alert.addAction(cancelAction)
-        presentViewController(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
     
-    func presentErrorAlert(error: String, completion: (() -> Void)?) {
-        let alert = UIAlertController(title: "Error!", message: error, preferredStyle: .Alert)
-        let okayAction = UIAlertAction(title: "Okay", style: .Default) { (_) in
+    func presentErrorAlert(_ error: String, completion: (() -> Void)?) {
+        let alert = UIAlertController(title: "Error!", message: error, preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "Okay", style: .default) { (_) in
             if let completion = completion {
                 completion()
             }
         }
         alert.addAction(okayAction)
-        presentViewController(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
     
     func presentEndOfPunsAlert() {
-        let alert = UIAlertController(title: "You're about to run out of puns!", message: "One More Pun only works if people like you submit puns! Tap the + to add one!", preferredStyle: .Alert)
-        let submitAction = UIAlertAction(title: "Let's go!", style: .Default) { (_) in
+        let alert = UIAlertController(title: "You're about to run out of puns!", message: "One More Pun only works if people like you submit puns! Tap the + to add one!", preferredStyle: .alert)
+        let submitAction = UIAlertAction(title: "Let's go!", style: .default) { (_) in
             self.punsReadCount = 0
             self.presentSubmitPunAlert(nil)
         }
-        let cancelAction = UIAlertAction(title: "No thanks", style: .Cancel) { (_) in
+        let cancelAction = UIAlertAction(title: "No thanks", style: .cancel) { (_) in
             self.punsReadCount = 0
         }
         alert.addAction(submitAction)
         alert.addAction(cancelAction)
-        presentViewController(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
     
     func showPunInfoActionSheet() {
-        let actionSheet = UIAlertController(title: "", message: "Options", preferredStyle: .ActionSheet)
-        let shareAction = UIAlertAction(title: "Share Pun", style: .Default) { (_) in
+        let actionSheet = UIAlertController(title: "", message: "Options", preferredStyle: .actionSheet)
+        let shareAction = UIAlertAction(title: "Share Pun", style: .default) { (_) in
             self.share()
         }
-        let googleAction = UIAlertAction(title: "Don't get it?", style: .Default) { (_) in
+        let googleAction = UIAlertAction(title: "Don't get it?", style: .default) { (_) in
             self.openGoogleForPun()
         }
-        let reportAction = UIAlertAction(title: "Report", style: .Default) { (_) in
+        let reportAction = UIAlertAction(title: "Report", style: .default) { (_) in
             self.checkIfCurrentUserIsNil({
                 self.presentReportPunAlert()
                 }, ifNil: { 
                     self.presentNoAccountAlert()
             })
         }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         actionSheet.addAction(shareAction)
         actionSheet.addAction(googleAction)
         actionSheet.addAction(reportAction)
         actionSheet.addAction(cancelAction)
-        presentViewController(actionSheet, animated: true, completion: nil)
+        present(actionSheet, animated: true, completion: nil)
     }
     
     func share() {
         let items = PunController.shared.getItemsToShare(pun, color: color)
         let activityViewController = UIActivityViewController(activityItems: items, applicationActivities: nil)
-        activityViewController.excludedActivityTypes = [UIActivityTypeCopyToPasteboard, UIActivityTypeAirDrop, UIActivityTypeAddToReadingList, UIActivityTypeAssignToContact, UIActivityTypePostToTencentWeibo, UIActivityTypePostToVimeo, UIActivityTypePrint, UIActivityTypePostToWeibo]
-        presentViewController(activityViewController, animated: true, completion: nil)
+        activityViewController.excludedActivityTypes = [UIActivityType.copyToPasteboard, UIActivityType.airDrop, UIActivityType.addToReadingList, UIActivityType.assignToContact, UIActivityType.postToTencentWeibo, UIActivityType.postToVimeo, UIActivityType.print, UIActivityType.postToWeibo]
+        present(activityViewController, animated: true, completion: nil)
     }
     
     func openGoogleForPun() {
-        let replaced = pun.body.stringByReplacingOccurrencesOfString(" ", withString: "+")
-        if let url = NSURL(string: "http://lmgtfy.com/?q=\(replaced)") {
-            UIApplication.sharedApplication().openURL(url)
+        let replaced = pun.body.replacingOccurrences(of: " ", with: "+")
+        if let url = URL(string: "http://lmgtfy.com/?q=\(replaced)") {
+            UIApplication.shared.openURL(url)
         }
     }
     
     func presentReportPunAlert() {
-        let alert = UIAlertController(title: "Report pun?", message: "Only use this feature if you want to report this pun as inappropriate or not a real pun.", preferredStyle: .Alert)
-        let reportAction = UIAlertAction(title: "Report", style: .Destructive) { (_) in
+        let alert = UIAlertController(title: "Report pun?", message: "Only use this feature if you want to report this pun as inappropriate or not a real pun.", preferredStyle: .alert)
+        let reportAction = UIAlertAction(title: "Report", style: .destructive) { (_) in
             PunController.shared.reportPun(self.pun)
             self.presentPunReportedConfirmation()
         }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alert.addAction(reportAction)
         alert.addAction(cancelAction)
-        presentViewController(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
     
     func presentPunReportedConfirmation() {
-        let alert = UIAlertController(title: "Pun Reported", message: "Your complaint has been recorded. Thank you for keeping One More Pun awesome!", preferredStyle: .Alert)
-        let okayAction = UIAlertAction(title: "Okay", style: .Default, handler: nil)
+        let alert = UIAlertController(title: "Pun Reported", message: "Your complaint has been recorded. Thank you for keeping One More Pun awesome!", preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
         alert.addAction(okayAction)
-        presentViewController(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
     
     func presentNoAccountAlert() {
-        let alert = UIAlertController(title: "Oops!", message: "Looks like you don't have an account. Please sign up or log in to use this feature.", preferredStyle: .Alert)
-        let signupLoginAction = UIAlertAction(title: "Sign up or login", style: .Default) { (_) in
+        let alert = UIAlertController(title: "Oops!", message: "Looks like you don't have an account. Please sign up or log in to use this feature.", preferredStyle: .alert)
+        let signupLoginAction = UIAlertAction(title: "Sign up or login", style: .default) { (_) in
             self.showLoginSignUpView()
         }
-        let cancelAction = UIAlertAction(title: "Never mind", style: .Cancel, handler: nil)
+        let cancelAction = UIAlertAction(title: "Never mind", style: .cancel, handler: nil)
         alert.addAction(signupLoginAction)
         alert.addAction(cancelAction)
-        presentViewController(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
 }
